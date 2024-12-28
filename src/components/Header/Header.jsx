47a1aec28ed171b2776179a7733d060bb21a2c10
Hamsa <hamsamuse.hm@gmail.com>
2024-12-28 16:45:19 +0000

    changed the cv from a downloadable cv to pdf in the browser CV

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import Link from "next/link";
 
-const pdfURL = "http://localhost:3000/cv.pdf";
+const pdfURL = "https://donhamsaportfolio.vercel.app/cv.pdf";
 
 export default function Header() {
   const [activeIndex, setActiveIndex] = useState(null); // Track the active item index
@@ -12,22 +12,32 @@ export default function Header() {
     setActiveIndex(index);
   };
 
-  const downloadFileAtURL = (url) => {
-    const fileName = url.split("/").pop();
-    const aTag = document.createElement("a");
-    aTag.href = url;
-    aTag.setAttribute("download", fileName);
-    document.body.appendChild(aTag);
-    aTag.click();
-    aTag.remove();
-  };
+  // const downloadFileAtURL = (url) => {
+  //   const fileName = url.split("/").pop();
+  //   const aTag = document.createElement("a");
+  //   aTag.href = url;
+  //   aTag.setAttribute("download", fileName);
+  //   document.body.appendChild(aTag);
+  //   aTag.click();
+  //   aTag.remove();
+  // };
 
   return (
     <div className="flex lg:justify-between justify-center  bg-blue ml-[30px] mr-[30px] ">
       <div></div>
       <div className="flex flex-row justify-between w-[340px] bg-[#EDE9E9] h-[45px] items-center rounded-[19px] pl-[4px] pr-[4px] mt-0 pt-[3px] pb-[3px] text-[14px] font-custom gap-5 ">
         {["About", "Projects", "Blogs"].map((item, index) => (
-          <Link href={item==='About' ? '/' : item==='Projects' ? '/projects' : '/blogs'} className=' h-full flex flex-grow ' key={index}>
+          <Link
+            href={
+              item === "About"
+                ? "/"
+                : item === "Projects"
+                ? "/projects"
+                : "/blogs"
+            }
+            className=" h-full flex flex-grow "
+            key={index}
+          >
             <p
               onClick={() => handleClick(index)} // Update active index on click
               className={`flex-grow text-center h-full flex items-center justify-center rounded-[19px] cursor-pointer
@@ -42,12 +52,11 @@ export default function Header() {
           </Link>
         ))}
       </div>
-      <div
-        className="lg:items-center lg:flex hidden cursor-pointer relative right-[10%]"
-        onClick={() => downloadFileAtURL(pdfURL)}
-      >
-        CV
-      </div>
+      <Link href="https://donhamsaportfolio.vercel.app/cv.pdf" className="lg:items-center lg:flex hidden cursor-pointer relative right-[10%]">
+        <div>
+          CV
+        </div>
+      </Link>
     </div>
   );
 }
