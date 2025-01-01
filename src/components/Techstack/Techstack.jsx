1d7c32c81f21a197863daf2651c9c3a22db71c17
Hamsa <hamsamuse.hm@gmail.com>
2025-01-01 12:54:10 +0000

    is this going to keep going- further mobile opt.

--- src/components/Techstack/Techstack.jsx
+++ src/components/Techstack/Techstack.jsx
@@ -1,13 +1,13 @@
 export default function Techstack() {
   return (
-    <div className=" lg:col-span-1 lg:row rounded-[30px]  lg:p-2 pt-[10px] flex flex-col   items-center justify-center lg:row-span-2 bg-gradient-to-br from-[#1c2b43] to-black lg:space-y-4 h-full lg:h-auto  gap-y-[20px] lg:gap-y-[5px]">
+    <div className=" lg:col-span-1 lg:row rounded-[30px]  lg:p-2 pt-[25px] pl-[12px] pr-[12px] flex flex-col   items-center justify-center lg:row-span-2 bg-gradient-to-br from-[#1c2b43] to-black lg:space-y-4 h-full lg:h-auto  gap-y-[20px] lg:gap-y-[5px]">
       <p className="font-techStack font-extrabold text-white text-[18px] ">
         Tech Stack
       </p>
       <div className="bg-[rgb(15,23,42)] rounded-[30px] h-max  text-[10px] p-[16px] bg-gradient-to-br from-[#1c2b43] to-black w-[100%]  ">
         <div className="flex items-center">
           <img src="/comp.png" className="w-[17px] h-[17px] mr-[8px] " />
-          <p className="text-[#94A3B8]">Programming languages and frameworks</p>
+          <p className="text-[#94A3B8] mb-[13px] lg:mb-0">Programming languages and frameworks</p>
         </div>
         <div className="flex flex-wrap space-y-[7px]  items-end space-x-[3px] ">
           <div className="bg-[rgba(45,212,191,0.1)] p-[5px] text-teal-400  rounded-full h-max ">
