271db90049ca946815a92c9b8ed1b19091c70eba
Hamsa <hamsamuse.hm@gmail.com>
2025-01-01 13:16:40 +0000

    readjusted the photo

--- src/components/ProfileImage/ProfileImage.jsx
+++ src/components/ProfileImage/ProfileImage.jsx
@@ -1,6 +1,6 @@
 export default function ProfileImage() {
   return (
-    <div className='lg:w-full lg:h-full h-[220px] rounded-[30px] overflow-hidden flex justify-center w-[100%]'>
+    <div className='lg:w-full lg:h-full h-[208px] rounded-[30px] overflow-hidden flex justify-center w-[100%]'>
       <img src="/ProfileImage.jpg" className="h-full lg:w-full w-[100%] rounded-[30px] "></img>
     </div>
   );
