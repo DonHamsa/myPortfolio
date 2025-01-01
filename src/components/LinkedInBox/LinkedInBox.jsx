9aedccbe1de1d455f8ecdce9bd34dd48671be04f
Hamsa <hamsamuse.hm@gmail.com>
2025-01-01 13:02:13 +0000

    linkedin box size decreased

--- src/components/LinkedInBox/LinkedInBox.jsx
+++ src/components/LinkedInBox/LinkedInBox.jsx
@@ -2,7 +2,7 @@ import { FaLinkedin } from "react-icons/fa";
 
 export default function LinkedInBox() {
   return (
-    <div className="bg-[rgb(10,102,194)] w-[100%] rounded-[30px] lg:h-[100%] h-[220px]  flex justify-center items-center relative ">
+    <div className="bg-[rgb(10,102,194)] w-[100%] rounded-[30px] lg:h-[100%] h-[204px]  flex justify-center items-center relative ">
       <a
         href="https://www.linkedin.com/in/hamsamuse/"
         className="h-full w-full flex justify-center items-center "
