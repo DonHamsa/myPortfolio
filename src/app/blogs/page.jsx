391892210eae7114083d774842ef4dc11ce37044
Hamsa <hamsamuse.hm@gmail.com>
2025-01-03 12:15:45 +0000

    little grammar update on blog

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -14,7 +14,7 @@ export default function Blogs() {
       <div className="lg:pr-[25%] lg:pl-[25%]  pl-[7%] pr-[7%] flex justify-center ">
         <BlogPreview
           heading="People look left, but rarely look right "
-          synopsis="Optimism and quick-thinking are skills that are applauded, you are praised being decisive and determined. However, sometimes this can lead to misreading a situation, calling something for what it isn't."
+          synopsis="Optimism and quick-thinking are skills that are applauded, you are praised for being decisive and determined. However, sometimes this can lead to misreading a situation, calling something for what it isn't."
           date="3rd Jan 2025"
           href="/blogs/beingReal"
         />
