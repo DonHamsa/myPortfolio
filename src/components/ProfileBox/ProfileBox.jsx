357053a2310eb4628f82933f1a671d3630432e2d
Hamsa <hamsamuse.hm@gmail.com>
2025-01-05 15:50:55 +0000

    added icons

--- src/components/ProfileBox/ProfileBox.jsx
+++ src/components/ProfileBox/ProfileBox.jsx
@@ -1,3 +1,6 @@
+import { CiLocationOn } from "react-icons/ci";
+import { MdOutlineWorkOutline } from "react-icons/md";
+
 export default function ProfileBox() {
   return (
     <div className="bg-white rounded-[30px]   flex flex-col  justify-center col-span-2 p-[35px] ">
@@ -6,17 +9,18 @@ export default function ProfileBox() {
         Software Developer
       </p>
       <div className="flex mb-[10px] ">
-        <p className="mr-[6px] bg-#F7F2F2 text-black rounded-15px text-[11px]  bg-gray-100 rounded-[40px] p-[8px]">
-          Based in London
-        </p>
-        <p className="bg-#F7F2F2 text-black rounded-15px text-[11px]   bg-gray-100 rounded-[40px] p-[8px]">
-          {" "}
-          Open to Work
-        </p>
+        <div className="flex flex-row mr-[6px] bg-#F7F2F2 text-black rounded-15px text-[11px]  bg-gray-100 rounded-[40px] p-[8px] justify-center text-center">
+          <CiLocationOn className="w-[14px] h-[14px] mr-[4px]  text-gray-400" />
+          <p className="">Based in London</p>
+        </div>
+        <div className="flex flex-row justify-center text-center bg-#F7F2F2 text-black rounded-15px text-[11px]   bg-gray-100 rounded-[40px] p-[8px]">
+          <MdOutlineWorkOutline className="w-[14px] h-[14px] mr-[4px] text-gray-400" />
+          <p className=""> Open to Work</p>
+        </div>
       </div>
       <p className="text-grey text-[10px] text-gray-500">
         A passionate developer focused on creating intuitive and impactful web
-        experiences. 
+        experiences.
       </p>
       <p className="mt-[6px] text-grey text-[10px]  text-gray-500">
         Currently exploring new opportunities and always excited to collaborate
