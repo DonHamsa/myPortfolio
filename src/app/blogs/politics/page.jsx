3f2d0a498a5c69874f0d3bdf99d83cee95a6b70d
Hamsa <hamsamuse.hm@gmail.com>
2025-01-18 11:15:15 +0000

    shorter paragraphs

--- src/app/blogs/politics/page.jsx
+++ src/app/blogs/politics/page.jsx
@@ -2,35 +2,93 @@ import PostTemplate from "@/components/PostTemplate/PostTemplate";
 
 export default function Journey() {
   return (
-    <PostTemplate
-      title="What even is Politics"
-      date="17th Jan 2025"
-    >
+    <PostTemplate title="What even is Politics" date="17th Jan 2025">
       <p className="mb-[19px] ">
-      When I see a horrible video on social media of a knife attack I instantly know what the comments will be filled with. There only seems to be one person to blame. Sadiq Khan. Now, Sadiq Khan is the elected Mayor of London and so he is responsible for a lot of things in London, like transport, housing, roads and the economy. Now, if I were to go down that list and take a guess at what the mayor would want, I would say great transport, a lot of houses, smooth,accessible roads and a strong and stable economy. I am sure everyone also wants the same. The problem is that it is just not realistic.
-
+        When I see a horrible video on social media of a knife attack I
+        instantly know what the comments will be filled with. There only seems
+        to be one person to blame. Sadiq Khan. Now, Sadiq Khan is the elected
+        Mayor of London and so he is responsible for a lot of things in London,
+        like transport, housing, roads and the economy. Now, if I were to go
+        down that list and take a guess at what the mayor would want, I would
+        say great transport, a lot of houses, smooth,accessible roads and a
+        strong and stable economy. I am sure everyone also wants the same. The
+        problem is that it is just not realistic.
       </p>
       <p className="mb-[19px] ">
-      So why is it Sadiq Khan’s fault? To me,  I cannot see how Sadiq Khan can be blamed. He did not order the stabbing. He clearly does not support people carrying knives with the aim to harm. A more reasonable response would be to ask what is being done to stop this. Logically, I would argue that we should impose harsher sentences for those convicted, so that those thinking of carrying a knife will think twice before purchasing or walking on the street with one.To pair logic with more logic, I say we do better at  catching those who perpetrate these attacks. So in summary, changing the sentencing laws around knife crime plus more or better police presence surely will shut the book on this nightmare of a story.
-
+        So why is it Sadiq Khan’s fault? To me, I cannot see how Sadiq Khan can
+        be blamed. He did not order the stabbing. He clearly does not support
+        people carrying knives with the aim to harm. A more reasonable response
+        would be to ask what is being done to stop this. Logically, I would
+        argue that we should impose harsher sentences for those convicted, so
+        that those thinking of carrying a knife will think twice before
+        purchasing or walking on the street with one.To pair logic with more
+        logic, I say we do better at catching those who perpetrate these
+        attacks. So in summary, changing the sentencing laws around knife crime
+        plus more or better police presence surely will shut the book on this
+        nightmare of a story.
       </p>
       <p className="mb-[19px] ">
-      Again, the sad reality is that neither really is a hopeful solution. Increasing sentencing for knife crime apparently does not solve the problem. This is not the average person’s opinion, but those we elect into parliament and the judiciary. To really tackle the issue, they say we need to know people’s reasons for carrying knives and remove them. I do not buy it. With every bad action, crime or immoral wrong, there is always something that makes you feel sorry for the bad guy, maybe even something that makes you relate to him, humanise him and see him as a normal person. You can go on social media today and you will find a group of Hitler fan boys, posting videos of his speeches, falling head over heels for his "aura". 
-      
-      I say we should not care for people’s backstory. Send the sentencing through the roof and you will see less people carrying knives. However, I do not think adding more police to the streets will work, doubling the numbers yes, but any minor changes to the policing budget will not do much.  Stop and Search in my opinion is a great strategy if implemented with the toughness and ruthlessness it so requires. I see it like this: you stop someone and they have a massive machete, you just stopped at least one person being killed or seriously harmed, if they have nothing on them, no problem, everyone carries on with their life. Of course, you will have bad people use it for bad intentions, but that is with everything. But applying that standard comes with baggage. Which means that ,as a strategy, today stop and search is good but not good enough.
-
+        Again, the sad reality is that neither really is a hopeful solution.
+        Increasing sentencing for knife crime apparently does not solve the
+        problem. This is not the average person’s opinion, but those we elect
+        into parliament and the judiciary. To really tackle the issue, they say
+        we need to know people’s reasons for carrying knives and remove them. I
+        do not buy it. With every bad action, crime or immoral wrong, there is
+        always something that makes you feel sorry for the bad guy, maybe even
+        something that makes you relate to him, humanise him and see him as a
+        normal person. You can go on social media today and you will find a
+        group of Hitler fan boys, posting videos of his speeches, falling head
+        over heels for his "aura".
       </p>
       <p className="mb-[19px] ">
-      And finally, ULEZ is probably the thing most people will remember Sadiq Khan for.  What was it about people wanting Sadiq Khan to improve London and make it better? Doesn’t everyone want cleaner air to breathe? But with the introduction of ULEZ, you would have thought Sadiq Khan was calling for a revolution. Most of the cars people drive are not even caught by ULEZ. So most people’s lives remain unaffected. Politicians ,mostly non-labour ones, were some of the loudest voices screaming about how bad it is. Last time I checked, every one of their manifestos talk about air quality and the need to do something about it. Weirdly when that talk becomes action, they are on the other side, campaigning to have it removed. And then we go on about the cost of living…..
-
+        I say we should not care for people’s backstory. Send the sentencing
+        through the roof and you will see less people carrying knives. However,
+        I do not think adding more police to the streets will work, doubling the
+        numbers yes, but any minor changes to the policing budget will not do
+        much. Stop and Search in my opinion is a great strategy if implemented
+        with the toughness and ruthlessness it so requires. I see it like this:
+        you stop someone and they have a massive machete, you just stopped at
+        least one person being killed or seriously harmed, if they have nothing
+        on them, no problem, everyone carries on with their life. Of course, you
+        will have bad people use it for bad intentions, but that is with
+        everything. But applying that standard comes with baggage. Which means
+        that ,as a strategy, today stop and search is good but not good enough.
       </p>
       <p className="mb-[19px] ">
-      I am going to try being logical here. For the past 15 years, whenever there have been key economic policy announcements, it felt like bad news. Every time. It makes the whole field sound illogical and too boring to get to the bottom of. Why can’t we just all take some time to think about what economics is: unlimited choice, limited resources. We all want no homelessness, a home for every person, free transport etc. But we can’t have all of that. Tough decisions have to be made. You can sell the long term vision but you cannot ignore the families going without heating in the winter. You can prioritise tax-cuts, reducing transport fares and increasing public spending, but the deficit must be reduced, which means nothing to the average person. It truly feels like whatever you do, people will complain and the journalist will make their money.
-
+        And finally, ULEZ is probably the thing most people will remember Sadiq
+        Khan for. What was it about people wanting Sadiq Khan to improve London
+        and make it better? Doesn’t everyone want cleaner air to breathe? But
+        with the introduction of ULEZ, you would have thought Sadiq Khan was
+        calling for a revolution. Most of the cars people drive are not even
+        caught by ULEZ. So most people’s lives remain unaffected. Politicians
+        ,mostly non-labour ones, were some of the loudest voices screaming about
+        how bad it is. Last time I checked, every one of their manifestos talk
+        about air quality and the need to do something about it. Weirdly when
+        that talk becomes action, they are on the other side, campaigning to
+        have it removed. And then we go on about the cost of living…..
       </p>
-      
+      <p className="mb-[19px] ">
+        I am going to try being logical here. For the past 15 years, whenever
+        there have been key economic policy announcements, it felt like bad
+        news. Every time. It makes the whole field sound illogical and too
+        boring to get to the bottom of. Why can’t we just all take some time to
+        think about what economics is: unlimited choice, limited resources. We
+        all want no homelessness, a home for every person, free transport etc.
+        But we can’t have all of that. Tough decisions have to be made. You can
+        sell the long term vision but you cannot ignore the families going
+        without heating in the winter. You can prioritise tax-cuts, reducing
+        transport fares and increasing public spending, but the deficit must be
+        reduced, which means nothing to the average person. It truly feels like
+        whatever you do, people will complain and the journalist will make their
+        money.
+      </p>
+
       <p className="mb-[19px] pb-[20px]">
-      As I get older, I will definitely change my opinion about Politics in general, but I just feel like it is the worst job in the world. It feels like you get blamed for everything. You can try your best, have the best intentions, but you will always be playing the blame game with the public and journalists. It is just boring, but the money is not too bad. 
+        As I get older, I will definitely change my opinion about Politics in
+        general, but I just feel like it is the worst job in the world. It feels
+        like you get blamed for everything. You can try your best, have the best
+        intentions, but you will always be playing the blame game with the
+        public and journalists. It is just boring, but the money is not too bad.
       </p>
     </PostTemplate>
   );
