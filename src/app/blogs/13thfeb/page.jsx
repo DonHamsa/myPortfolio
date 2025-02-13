ec293a9644b00426d6fedbe74bfbd25b1d5bb67a
Hamsa <hamsamuse.hm@gmail.com>
2025-02-13 18:38:19 +0000

    new post

---
+++ src/app/blogs/13thfeb/page.jsx
@@ -0,0 +1,56 @@
+import PostTemplate from "@/components/PostTemplate/PostTemplate";
+
+export default function GoodApple() {
+  return (
+    <PostTemplate title="Falling at the last hurdle" date="13th Feb 2025">
+      <p className="mb-[19px]">
+        I feel like shit right now. I wonder what it must feel like to lose
+        someone you really love or care about.
+      </p>
+
+      <p className="mb-[19px]">
+        After a few weeks of putting all my effort and energy and hope really
+        into getting a place at Version 1, I got the email of rejection. It
+        hurt. It stung. I felt defeated. Your mind goes everywhere. Where did I
+        fall short? Two things were happening in my mind. First, I felt like I
+        lost my pride, my ego and self-confidence. To be deemed to be a weaker
+        candidate did not feel good. But after a few seconds of zoning-out, it
+        was the 'how could I have done better' question that repeated a billion
+        and one times in my head. The problem is without feedback, I won't be
+        satisfied with the answer. I would drive myself psycho before arriving
+        at a final conclusion.
+      </p>
+
+      <p className="mb-[19px]">
+        The path forward looks less brighter than it did a few days ago. But I
+        wait. I wait for the feedback to improve. To pick myself up, dust my
+        self down and come back a stronger and more improved candidate. And if I
+        fall back again. I repeat. Repeat and then repeat again.
+      </p>
+      <p className="mb-[19px]">
+        It is not the right time to be in deep thought about the activities. I
+        thought I asked good questions. Questions that were the result of hours
+        of research into the company and whose answers were not available
+        anywhere but from those who worked and walked the halls of the offices.
+        The Lego activity my guess is where I underperformed. But to guess is
+        pointless. I just have to wait to hear the feedback and action on it for
+        a next time.
+      </p>
+      <p className="mb-[19px]">
+        Now I am really tempted to open a can of worms and explore what 'the
+        next time' means and talk about companies' hiring practices, the current
+        flatlining tech market, the trend toward a tech career to leave behind
+        what always seems to be a 'boring', 'unstimulating' previous job. But I
+        cannot really be asked to focus on that. I think people have a habit of
+        glamourising the past. I'm sure there was a high level of difficulty in
+        the past in securing a job in tech or elsewhere really.
+      </p>
+      <p className="mb-[19px] pb-[19px]">
+        So the race continues, the competition is on. It is on me to win, and
+        there are many battles ahead to prove myself. For now I failed, I was
+        poor and not good enough. But the beauty of life, is that you get a chance to be
+        victorious every second of every day until your last, dying breath.
+      </p>
+    </PostTemplate>
+  );
+}
--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -3,10 +3,18 @@ import BlogPreview from "@/components/BlogPreview/BlogPreview";
 export default function Blogs() {
   return (
     <>
+    <div className="lg:pr-[25%] lg:pl-[25%]  pl-[7%] pr-[7%] flex justify-center mb-[20px] lg:h-[300px] h-[370px]  ">
+        <BlogPreview
+          heading="Falling at the final hurdle"
+          synopsis="I wanted it. I needed it. I worked towards it. I did not get it. The inquest should not begin now. It will begin when I have all the information and the sadness subsides. However, putting thoughts into writing I think does help a bit."
+          date="13th Feb 2025"
+          href="blogs/13thfeb"
+        />
+      </div>
       <div className="lg:pr-[25%] lg:pl-[25%]  pl-[7%] pr-[7%] flex justify-center mb-[20px] lg:h-[300px] h-[370px]  ">
         <BlogPreview
           heading="The end of January"
-          synopsis="This month has felt the weirdest out of the last 18 months. I just finished a 3-month project and I feel like I have stepped right into chaos.I still have structure in my days, but this job could not come any sooner...  "
+          synopsis="This month has felt the weirdest out of the last 18 months. I just finished a 3-month project and I feel like I have stepped right into chaos.I still have structure in my days, but this job could not come any sooner."
           date="30th Jan 2025"
           href="blogs/endOfJan"
         />
@@ -14,7 +22,7 @@ export default function Blogs() {
       <div className="lg:pr-[25%] lg:pl-[25%]  pl-[7%] pr-[7%] flex justify-center mb-[20px] lg:h-[300px] h-[370px]  ">
         <BlogPreview
           heading="What even is Politics?"
-          synopsis="Democracy should mean every one shuts up most of the time. The elected people doing the work of those who elected them. Brexit, Labour, Conservatives. The noise suggests we have dictators in charge...."
+          synopsis="Democracy should mean every one shuts up most of the time. The elected people doing the work of those who elected them. Brexit, Labour, Conservatives. The noise suggests we have dictators in charge."
           date="17th Jan 2025"
           href="blogs/politics"
         />
@@ -22,7 +30,7 @@ export default function Blogs() {
       <div className="lg:pr-[25%] lg:pl-[25%] mb-[20px] pl-[7%] pr-[7%] flex justify-center lg:h-[300px] h-[370px]  ">
         <BlogPreview
           heading="Why I went from hating to loving Apple"
-          synopsis="I always feel a bit ashamed when I have to change an opinion. This is because there is a hint of pride with all of our opinions, a feeling of pity for those who are on the other side... "
+          synopsis="I always feel a bit ashamed when I have to change an opinion. This is because there is a hint of pride with all of our opinions, a feeling of pity for those who are on the other side."
           date="10th Jan 2025"
           href="/blogs/goodApple"
         />
@@ -30,7 +38,7 @@ export default function Blogs() {
       <div className="lg:pr-[25%] lg:pl-[25%]  pl-[7%] pr-[7%] flex justify-center mb-[20px] lg:h-[300px]  h-[390px]  ">
         <BlogPreview
           heading="Why I am optimistic in my job hunt "
-          synopsis="Rejection after rejection will make anyone doubt their ability, their self-speech about pushing forward regardless of what the email response reads. It is always easier said that done, with anything really...."
+          synopsis="Rejection after rejection will make anyone doubt their ability, their self-speech about pushing forward regardless of what the email response reads. It is always easier said that done, with anything really."
           date="31st Dec 2024"
           href="blogs/journey"
         />
